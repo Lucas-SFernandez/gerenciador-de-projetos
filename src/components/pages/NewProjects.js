65ab52805b24c5e43f8d3f367443a00cdf85b778
Lucas-SFernandez <127568078+Lucas-SFernandez@users.noreply.github.com>
2023-12-12 20:38:54 -0300

    adicionando pseudo banco de dados para poder realizar a adição de projetos

--- src/components/pages/NewProjects.js
+++ src/components/pages/NewProjects.js
@@ -1,20 +1,50 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './NewProjects.module.css'
 
 import ProjetoForm from '../project/ProjetoForm'
 
-function NewProjects(){
+function NewProjects() {
+
+    const navigate = useNavigate()
+
+    function creatPost(project) {
+
+        //initialize cost an services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                // redirect
+
+                navigate('/projetos', { message: 'Projeto criado com sucesso!' })
+            })
+            .catch(err => console.log(err))
+
+    }
+
+
     return (
-    <div className={styles.novosprojetos_container}>
-        <h1>Criar Projeto</h1>
+        <div className={styles.novosprojetos_container}>
+            <h1>Criar Projeto</h1>
 
-        <p>
-            Crie seu projeto para depois adicionar os serviçoes
-        </p>
+            <p>
+                Crie seu projeto para depois adicionar os serviçoes
+            </p>
 
-        <ProjetoForm btnText="Criar Projeto"/>
+            <ProjetoForm handleSubmit={creatPost} btnText="Criar Projeto" />
 
 
-    </div>
+        </div>
     )
 }
 
--- src/components/project/ProjetoForm.js
+++ src/components/project/ProjetoForm.js
@@ -1,21 +1,63 @@
+import { useState, useEffect } from 'react'
+
 import styles from './ProjetoForm.module.css'
 
 import Input from '../form/Input'
 
 import Select from '../form/Select'
+
 import SubmitButton from '../form/SubmitButton'
 
-function ProjetoForm({btnText}) {
+function ProjetoForm({ handleSubmit, btnText, projectData }) {
+
+    const [categories, setCategories] = useState([])
+
+    const [project, setProject] = useState(projectData || {})
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value})
+    }
+
+
+    function handleCategory(e) {
+        setProject({ ...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+        },
+    })
+    }
+
+
     return (
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
 
-            <Input type="text" name="name" text="Nome do Projeto" placeholder="Insira o nome do projeto" />
+            <Input type="text" name="name" text="Nome do Projeto" placeholder="Insira o nome do projeto" handleOnChange={handleChange} value={project.name ? project.name : ''}/>
 
-            <Input type="number" name="budget" text="Orçamento do Projeto" placeholder="Insira o orçamento total do projeto" />
+            <Input type="number" name="budget" text="Orçamento do Projeto" placeholder="Insira o orçamento total do projeto" handleOnChange={handleChange} value={project.budget ? project.budget : ''}/>
 
-            <Select name="category_id" text="Selecione a Categoria" />
+            <Select name="category_id" text="Selecione a Categoria" options={categories} handleOnChange={handleCategory} value={project.category ? project.category.id : ''}/>
 
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
 
         </form>
     )
